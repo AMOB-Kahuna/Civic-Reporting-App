dcff0425002061af720311aa43442c3f71afac98
AMOB-Kahuna <balloabdulmujeeb42@gmail.com>
2026-08-14 20:49:36 +0100

    Add reports route and make the pages minimum height the screen size

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Terms from './pages/Terms';
 import Layout from './components/Layout';
 import MakeReport from './pages/MakeReport';
+import Reports from './pages/Reports';
 
 function App() {
 
@@ -13,7 +14,8 @@ function App() {
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
           <Route path='make-report' element={<MakeReport />} />
-          <Route path="/terms" element={<Terms />} />
+          <Route path='reports' element={<Reports />} />
+          <Route path="terms" element={<Terms />} />
         </Route>
       </Routes>
     </>
--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -5,13 +5,13 @@ import Footer from './Footer'
 
 const Layout = () => {
   return (
-    <>
+    <div className='min-h-screen flex flex-col justify-between'>
       <Header />
       <main className='px-5 py-5 text-xl'>
         <Outlet />
       </main>
       <Footer />
-    </>
+    </div>
   )
 }
 
